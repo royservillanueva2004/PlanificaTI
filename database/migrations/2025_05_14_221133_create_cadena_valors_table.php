af769ae8cc9cd03be07ff83d9ffee527e3502827
dennisdhm7 <chrhinojosam@upt.pe>
2025-05-14 17:21:40 -0500

    Proyecto Laravel PlanificaTI - configuración inicial

---
+++ database/migrations/2025_05_14_221133_create_cadena_valors_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cadena_valors', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cadena_valors');
+    }
+};
